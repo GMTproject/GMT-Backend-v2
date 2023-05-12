d421055e60dbda1a65406bc2cf1306f3d827ac01
ani2689 <ojno7777@gmail.com>
2023-05-12 19:29:36 +0900

    :tada: gmt 서버 v2 시작!

---
+++ gmt/src/test/java/com/pmn/gmt/GmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmn.gmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
