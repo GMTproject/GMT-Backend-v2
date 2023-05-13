41f03903f6cbfc4eec5ee2d0a6f94ccea9b7d406
shk0625 <tjfgml1238@gmail.com>
2023-05-13 16:53:30 +0900

    :seedling: FilterDTO
    FilterDTO (free,major,skill)

---
+++ src/main/java/com/pmn/gmt/domain/teacher/domain/dto/FilterDTO.java
@@ -0,0 +1,14 @@
+package com.pmn.gmt.domain.teacher.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FilterDTO {
+    private String free;
+    private String major;
+    private String skill;
+
+
+}
