296b3340c008ba26676840247a82667bf00edc31
ani2689 <ojno7777@gmail.com>
2023-05-15 10:57:26 +0900

    :recycle: FilterDto 불린값으로 수정

---
+++ src/main/java/com/pmn/gmt/domain/teacher/domain/dto/requestDto/FilterRequestDto.java
@@ -0,0 +1,15 @@
+package com.pmn.gmt.domain.teacher.domain.dto.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FilterRequestDto {
+    private boolean free;
+    private boolean major;
+    private boolean skill;
+    private boolean grade1;
+    private boolean grade2;
+    private boolean grade3;
+}
