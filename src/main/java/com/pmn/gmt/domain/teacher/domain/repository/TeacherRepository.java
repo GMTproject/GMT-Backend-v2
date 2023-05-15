0ba2930b4e8a3c78e2f2cf560b5131e64c69f597
ani2689 <ojno7777@gmail.com>
2023-05-15 10:57:54 +0900

    :sparkles: teacher repo 생성

---
+++ src/main/java/com/pmn/gmt/domain/teacher/domain/repository/TeacherRepository.java
@@ -0,0 +1,8 @@
+package com.pmn.gmt.domain.teacher.domain.repository;
+
+import com.pmn.gmt.domain.teacher.domain.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher,Integer> {
+
+}
