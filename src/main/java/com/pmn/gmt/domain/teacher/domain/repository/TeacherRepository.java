e67e7e5e70116f562cbae9fa4ab1acece4d8ddb6
ani2689 <ojno7777@gmail.com>
2023-05-15 15:56:39 +0900

    :sparkles: 필터를 위한 메서드 생성

--- src/main/java/com/pmn/gmt/domain/teacher/domain/repository/TeacherRepository.java
+++ src/main/java/com/pmn/gmt/domain/teacher/domain/repository/TeacherRepository.java
@@ -2,7 +2,15 @@ package com.pmn.gmt.domain.teacher.domain.repository;
 
 import com.pmn.gmt.domain.teacher.domain.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TeacherRepository extends JpaRepository<Teacher,Integer> {
+    List<Teacher> findByFreeIsNotNull();
+    List<Teacher> findByMajorIsNotNull();
+    List<Teacher> findBySkillIsNotNull();
 
+    List<Teacher> findByposition(String position);
 }
