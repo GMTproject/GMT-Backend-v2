577abee6306464e5ea97ec93ab027a7a1d844f93
ani2689 <ojno7777@gmail.com>
2023-05-22 08:22:57 +0900

    :sparkles: 필터 파라미터에 따라 선생님 리스트 도출

--- src/main/java/com/pmn/gmt/domain/teacher/controller/TeacherController.java
+++ src/main/java/com/pmn/gmt/domain/teacher/controller/TeacherController.java
@@ -1,13 +1,14 @@
 package com.pmn.gmt.domain.teacher.controller;
 
+import com.pmn.gmt.domain.teacher.domain.dto.requestDto.FilterRequestDto;
 import com.pmn.gmt.domain.teacher.domain.dto.responseDto.TeacherResponseDto;
 import com.pmn.gmt.domain.teacher.service.FindAllTeacherService;
+import com.pmn.gmt.domain.teacher.service.FindTeachersByFilterService;
 import com.pmn.gmt.domain.teacher.util.TeacherConverter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,10 +20,12 @@ public class TeacherController {
 
     private final FindAllTeacherService findAllTeacherService;
     private final TeacherConverter teacherConverter;
+    private final FindTeachersByFilterService findTeachersByFilterService;
 
-    public TeacherController(FindAllTeacherService findAllTeacherService, TeacherConverter teacherConverter) {
+    public TeacherController(FindAllTeacherService findAllTeacherService, FindTeachersByFilterService findTeachersByFilterService, TeacherConverter teacherConverter) {
         this.findAllTeacherService = findAllTeacherService;
         this.teacherConverter = teacherConverter;
+        this.findTeachersByFilterService =  findTeachersByFilterService;
     }
     @GetMapping
     public ResponseEntity<List<TeacherResponseDto>> findAllTeacher() {
@@ -31,6 +34,12 @@ public class TeacherController {
                 .collect(Collectors.toList()));
     }
 
+    @GetMapping("/filter")
+    public ResponseEntity<List<TeacherResponseDto>> findTeachersByFilterService(FilterRequestDto filterRequestDto) {
+        return ResponseEntity.ok(findTeachersByFilterService.execute(teacherConverter.toDto(filterRequestDto)).stream()
+                .map(teacherConverter::toResponseDto)
+                .collect(Collectors.toList()));
+    }
 
 
 }
