0148cae2a1e5e4edf2476ac43d2f9fd4bc98a5a1
ani2689 <ojno7777@gmail.com>
2023-05-30 10:10:39 +0900

    :sparkles: 에러 코드 정의

---
+++ src/main/java/com/pmn/gmt/global/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.pmn.gmt.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    UNAUTHENTICATED_USER(401, "인증되지 않은 사용자입니다."),
+
+    INVALID_FILTER(404,"유효하지 않은 필터입니다."),
+    NOT_FOUND_TEACHER(404,"유효하지 않은 교사입니다."),
+
+    NOT_FOUND_CLASSROOM(404,"유효하지 않은 교실입니다."),
+
+    INTERNAL_SERVER_ERROR(500,"서버 에러");
+
+    private final int status;
+    private final String description;
+}
