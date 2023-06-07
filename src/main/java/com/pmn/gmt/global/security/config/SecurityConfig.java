c999e8c8ee22b632afe5c7cc4d7b4115b6d29fa5
ani2689 <ojno7777@gmail.com>
2023-06-07 23:53:27 +0900

    :lock: 시큐리티 작업

---
+++ src/main/java/com/pmn/gmt/global/security/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.pmn.gmt.global.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        return (username) -> null;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .cors().and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/map").permitAll()
+
+                .antMatchers(HttpMethod.GET,"/teachers/**").hasAnyRole("TEACHER", "STUDENT")
+
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.PATCH, "/auth").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/auth").authenticated()
+
+                .anyRequest().permitAll()
+                .and()
+                .exceptionHandling()
+                .and()
+                .anonymous().authorities("GUEST")
+                .and()
+                .logout();
+
+        return http.build();
+
+    }
+}
