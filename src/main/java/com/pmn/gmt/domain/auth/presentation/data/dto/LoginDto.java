21d7795b8549e55649698baa03d34a3b19099b15
ani2689 <ojno7777@gmail.com>
2023-06-07 23:52:33 +0900

    :sparkles: 로그인 DTO 정의

---
+++ src/main/java/com/pmn/gmt/domain/auth/presentation/data/dto/LoginDto.java
@@ -0,0 +1,10 @@
+package com.pmn.gmt.domain.auth.presentation.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginDto {
+    String code;
+}
