d23ef33a8d56dc8172f7306585d00ca0d17929c9
ani2689 <ojno7777@gmail.com>
2023-06-07 21:19:51 +0900

    :sparkles: 유저 레포지토리 정의

---
+++ src/main/java/com/pmn/gmt/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pmn.gmt.domain.user.domain.repository;
+
+import com.pmn.gmt.domain.user.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByEmail(String email);
+}
