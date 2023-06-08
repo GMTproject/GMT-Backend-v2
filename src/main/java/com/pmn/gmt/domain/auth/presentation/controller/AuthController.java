aaf309825d31a61c08e7794fd50218b01adea4d7
ani2689 <ojno7777@gmail.com>
2023-06-08 20:48:19 +0900

    :sparkles: url 던져주는 서비스, 로그아웃 서비스 도입

--- src/main/java/com/pmn/gmt/domain/auth/presentation/controller/AuthController.java
+++ src/main/java/com/pmn/gmt/domain/auth/presentation/controller/AuthController.java
@@ -1,9 +1,14 @@
 package com.pmn.gmt.domain.auth.presentation.controller;
 
 import com.pmn.gmt.domain.auth.presentation.data.reqeuestDto.LoginRequestDto;
+import com.pmn.gmt.domain.auth.presentation.data.responseDto.LoginResponseDto;
+import com.pmn.gmt.domain.auth.presentation.data.responseDto.LoginUrlResponseDto;
 import com.pmn.gmt.domain.auth.service.LoginService;
+import com.pmn.gmt.domain.auth.service.LoginUrlService;
+import com.pmn.gmt.domain.auth.service.LogoutService;
 import com.pmn.gmt.domain.auth.util.AuthConverter;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
@@ -13,13 +18,25 @@ import javax.validation.Valid;
 public class AuthController {
 
     LoginService loginService;
+    LogoutService logoutService;
+
+    LoginUrlService loginUrlService;
     AuthConverter authConvertor;
 
+    @GetMapping
+    public ResponseEntity<LoginUrlResponseDto> loginUrl(){
+        return ResponseEntity.ok(loginUrlService.execute());
+    }
+
     @GetMapping("/login")
-    public String login(@RequestParam(value = "code") String code) {
-        System.out.println("실행");
-        loginService.execute(authConvertor.toDto(code));
-        return "redirect:/success";
+    public ResponseEntity<LoginResponseDto> login(@RequestParam(value = "code") String code) {
+        return ResponseEntity.ok(loginService.execute(authConvertor.toDto(code)));
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(){
+        logoutService.execute();
+        return ResponseEntity.noContent().build();
     }
 
 }
