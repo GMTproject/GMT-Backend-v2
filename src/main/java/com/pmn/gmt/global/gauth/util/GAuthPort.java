85db7f7312850536dd6c3ba2f92727300d3e2781
ani2689 <ojno7777@gmail.com>
2023-06-08 20:47:07 +0900

    :sparkles: gauth 프로퍼티 작업

--- src/main/java/com/pmn/gmt/global/gauth/util/GAuthPort.java
+++
@@ -1,9 +0,0 @@
-package com.pmn.gmt.global.gauth.util;
-
-import gauth.GAuthToken;
-import gauth.GAuthUserInfo;
-
-public interface GAuthPort {
-    GAuthToken queryGAuthToken(String code);
-    GAuthUserInfo queryGAuthUserInfo(String accessToken);
-}
--- src/main/java/com/pmn/gmt/global/gauth/util/GAuthProperties.java
+++ src/main/java/com/pmn/gmt/global/gauth/util/GAuthProperties.java
@@ -1,12 +1,21 @@
 package com.pmn.gmt.global.gauth.util;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Getter
-@AllArgsConstructor
+@Component
 public class GAuthProperties {
-    private String clientId;
-    private String clientSecret;
-    private String redirectUrl;
+    private final String clientId;
+    private final String clientSecret;
+    private final String redirectUri;
+
+    public GAuthProperties(@Value("${gauth.clientId}") String clientId,
+                           @Value("${gauth.clientSecret}") String clientSecret,
+                           @Value("${gauth.redirectUri}")String redirectUri) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
+    }
 }
