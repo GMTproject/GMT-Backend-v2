3c9f79c5e0d8f692318fa71951981a21796b6338
ani2689 <ojno7777@gmail.com>
2023-06-08 20:44:24 +0900

    :heavy_plus_sign: redis 종속성 추가

--- src/main/java/com/pmn/gmt/domain/auth/domain/entity/RefreshToken.java
+++ src/main/java/com/pmn/gmt/domain/auth/domain/entity/RefreshToken.java
@@ -1,27 +1,19 @@
 package com.pmn.gmt.domain.auth.domain.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @RedisHash(value = "refreshToken", timeToLive = 60L * 60 * 24 * 7)
 @Getter
 public class RefreshToken {
 
     @Id
-    private int id;
-
-    private String token;
-
     private int userId;
 
-    public RefreshToken(int userId, String token) {
-        this.userId = userId;
-        this.token = token;
-    }
+    private String token;
 }
