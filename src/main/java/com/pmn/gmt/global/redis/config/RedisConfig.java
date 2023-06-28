547ccb984d3b69ff439a31d2e2aad5479e441fce
shk0625 <tjfgml1238@gmail.com>
2023-06-28 12:02:23 +0900

    Redis Repo 사용하는 어노테이션 추가

--- src/main/java/com/pmn/gmt/global/redis/config/RedisConfig.java
+++ src/main/java/com/pmn/gmt/global/redis/config/RedisConfig.java
@@ -7,13 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Component;
 
 @Configuration
 @NoArgsConstructor
-@Component
+@EnableRedisRepositories
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
