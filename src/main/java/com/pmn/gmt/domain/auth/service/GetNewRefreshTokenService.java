b8bc63e86860fbc03e427f7ba6d42aaaae03df4f
ani2689 <ojno7777@gmail.com>
2023-07-30 22:36:29 +0900

    :bug: 반환타입 수정

--- src/main/java/com/pmn/gmt/domain/auth/service/GetNewRefreshTokenService.java
+++ src/main/java/com/pmn/gmt/domain/auth/service/GetNewRefreshTokenService.java
@@ -1,7 +1,8 @@
 package com.pmn.gmt.domain.auth.service;
 
 import com.pmn.gmt.domain.auth.presentation.data.dto.RefreshTokenDto;
+import com.pmn.gmt.domain.auth.presentation.data.responseDto.RefreshTokenResponseDto;
 
 public interface GetNewRefreshTokenService {
-    RefreshTokenDto execute(RefreshTokenDto refreshTokenDto);
+    RefreshTokenResponseDto execute(RefreshTokenDto refreshTokenDto);
 }
