4fca01197f22fdfb4aa8ecc9303f49e271a2eeb5
ani2689 <ojno7777@gmail.com>
2023-08-14 22:15:53 +0900

    :sparkles: Dto 정의

---
+++ src/main/java/com/pmn/gmt/domain/user/presentation/data/responseDto/UserInfoResponseDto.java
@@ -0,0 +1,10 @@
+package com.pmn.gmt.domain.user.presentation.data.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoResponseDto {
+    private String name;
+}
