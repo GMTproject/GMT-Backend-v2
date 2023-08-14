3303672d12fa5eb4db6473af21ebd61b959809d7
ani2689 <ojno7777@gmail.com>
2023-08-14 22:27:47 +0900

    :sparkles: 컨버터 기능 추가

--- src/main/java/com/pmn/gmt/domain/user/util/UserConverter.java
+++ src/main/java/com/pmn/gmt/domain/user/util/UserConverter.java
@@ -2,8 +2,13 @@ package com.pmn.gmt.domain.user.util;
 
 import com.pmn.gmt.domain.user.domain.entity.User;
 import com.pmn.gmt.domain.user.domain.entity.role.UserRole;
+import com.pmn.gmt.domain.user.presentation.data.dto.UserInfoDto;
+import com.pmn.gmt.domain.user.presentation.data.responseDto.UserInfoResponseDto;
 import gauth.GAuthUserInfo;
 
 public interface UserConverter {
     User toEntity(GAuthUserInfo gAuthUserInfo, UserRole role);
+    UserInfoDto toDto(User user);
+
+    UserInfoResponseDto toResponseDto(UserInfoDto userInfoDto);
 }
