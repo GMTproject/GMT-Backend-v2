d3f0c878bffd2f4a0d4f0069da8ab78fcd32651b
ani2689 <ojno7777@gmail.com>
2023-08-14 22:29:08 +0900

    :sparkles: 로그인된 유저 가져오는 서비스 추상화 및 구현

---
+++ src/main/java/com/pmn/gmt/domain/user/service/FindUserInfoService.java
@@ -0,0 +1,7 @@
+package com.pmn.gmt.domain.user.service;
+
+import com.pmn.gmt.domain.user.presentation.data.dto.UserInfoDto;
+
+public interface FindUserInfoService {
+    UserInfoDto execute();
+}
