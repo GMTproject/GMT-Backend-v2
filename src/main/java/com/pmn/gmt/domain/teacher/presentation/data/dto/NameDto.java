898f1b14ae8dcc8bb864da1376f5f765558e23bf
shk0625 <tjfgml1238@gmail.com>
2023-08-21 10:28:05 +0900

    🔭NameDto

---
+++ src/main/java/com/pmn/gmt/domain/teacher/presentation/data/dto/NameDto.java
@@ -0,0 +1,11 @@
+package com.pmn.gmt.domain.teacher.presentation.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NameDto {
+    // 데이터베이스 쿼리문 적을거임!
+    public String name;
+}
