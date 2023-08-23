f879548ebd7af44339d195290a16ca7a6f4317bd
shk0625 <tjfgml1238@gmail.com>
2023-08-23 10:04:46 +0900

    💫 NameRequestDto

---
+++ src/main/java/com/pmn/gmt/domain/teacher/presentation/data/requestDto/NameRequestDto.java
@@ -0,0 +1,10 @@
+package com.pmn.gmt.domain.teacher.presentation.data.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class NameRequestDto {
+    private String name;
+}
