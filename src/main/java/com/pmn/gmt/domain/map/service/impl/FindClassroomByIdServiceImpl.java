c2dae7d2dd26966d283f74c0f0e26c5019081410
ani2689 <ojno7777@gmail.com>
2023-09-01 00:35:37 +0900

    :sparkles: 맵 아이디로 서비스 찾기

---
+++ src/main/java/com/pmn/gmt/domain/map/service/impl/FindClassroomByIdServiceImpl.java
@@ -0,0 +1,26 @@
+package com.pmn.gmt.domain.map.service.impl;
+
+import com.pmn.gmt.domain.map.domain.repository.MapRepository;
+import com.pmn.gmt.domain.map.exception.ClassRoomNotFoundException;
+import com.pmn.gmt.domain.map.presentation.data.dto.MapDto;
+import com.pmn.gmt.domain.map.presentation.data.dto.MapIdDto;
+import com.pmn.gmt.domain.map.service.FindClassroomByIdService;
+import com.pmn.gmt.domain.map.util.MapConverter;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class FindClassroomByIdServiceImpl implements FindClassroomByIdService {
+
+    private final MapRepository mapRepository;
+    private final MapConverter mapConverter;
+
+    @Override
+    public MapDto execute(MapIdDto mapIdDto) {
+        if(mapRepository.findById(mapIdDto.getMapId()).isPresent()){
+            return mapConverter.toDto(mapRepository.findById(mapIdDto.getMapId()).get());
+        }else
+            throw new ClassRoomNotFoundException();
+    }
+}
