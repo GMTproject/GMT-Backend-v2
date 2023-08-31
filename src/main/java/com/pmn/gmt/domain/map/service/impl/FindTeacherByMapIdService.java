ad610d21b77375bb755d6a174310f59ea537efad
ani2689 <ojno7777@gmail.com>
2023-09-01 00:06:02 +0900

    :sparkles: 서비스 구현

---
+++ src/main/java/com/pmn/gmt/domain/map/service/impl/FindTeacherByMapIdService.java
@@ -0,0 +1,32 @@
+package com.pmn.gmt.domain.map.service.impl;
+
+import com.pmn.gmt.domain.map.domain.repository.MapRepository;
+import com.pmn.gmt.domain.map.exception.ClassRoomNotFoundException;
+import com.pmn.gmt.domain.map.presentation.data.dto.MapIdDto;
+import com.pmn.gmt.domain.map.presentation.data.dto.TeacherDto;
+import com.pmn.gmt.domain.map.service.FindTeachersByMapIdService;
+import com.pmn.gmt.domain.map.util.MapConverter;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class FindTeacherByMapIdService implements FindTeachersByMapIdService {
+
+    private final MapRepository mapRepository;
+    private final MapConverter mapConverter;
+
+    @Override
+    public List<TeacherDto> execute(MapIdDto mapIdDto) {
+        if(mapRepository.findById(mapIdDto.getMapId()).isPresent()){
+            return mapRepository.findById(mapIdDto.getMapId()).get()
+                    .getTeacher().stream()
+                    .map(mapConverter::toDto)
+                    .collect(Collectors.toList());
+        }else
+            throw new ClassRoomNotFoundException();
+    }
+}
