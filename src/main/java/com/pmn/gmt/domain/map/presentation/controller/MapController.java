14e89c545638c777762ae381c0eda7e438fced7e
ani2689 <ojno7777@gmail.com>
2023-08-31 23:33:24 +0900

    :sparkles: map 컨트롤러 생성

---
+++ src/main/java/com/pmn/gmt/domain/map/presentation/controller/MapController.java
@@ -0,0 +1,30 @@
+package com.pmn.gmt.domain.map.presentation.controller;
+
+import com.pmn.gmt.domain.map.service.FindTeachersByMapId;
+import com.pmn.gmt.domain.map.util.MapConverter;
+import com.pmn.gmt.domain.teacher.presentation.data.responseDto.TeacherResponseDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/map")
+@AllArgsConstructor
+public class MapController {
+
+    private final FindTeachersByMapId findTeachersByMapId;
+    private final MapConverter mapConverter;
+
+    @GetMapping("/{map_id}/teachers")
+    ResponseEntity<List<TeacherResponseDto>> findTeachersByMapId(@PathVariable("map_id") String mapId){
+        return ResponseEntity.ok((findTeachersByMapId.execute(mapConverter.toDto(mapId))).stream()
+                .map(mapConverter::toResponseDto)
+                .collect(Collectors.toList()));
+    }
+}
