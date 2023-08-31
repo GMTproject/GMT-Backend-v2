c71d5e66daec96b8ca678533c6a0d540381003e6
ani2689 <ojno7777@gmail.com>
2023-09-01 00:07:27 +0900

    :bug: 잘못 정의해쒀

--- src/main/java/com/pmn/gmt/domain/map/domain/repository/MapRepository.java
+++ src/main/java/com/pmn/gmt/domain/map/domain/repository/MapRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MapRepository extends JpaRepository<Integer, Map> {
+public interface MapRepository extends JpaRepository<Map, Integer> {
 }
