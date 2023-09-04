a54e00b04f7e5a932cd794fbaaeef11a00c4f0af
ani2689 <ojno7777@gmail.com>
2023-09-04 16:05:39 +0900

    :recycle: 리스트를 반환하도록 변경

--- src/main/java/com/pmn/gmt/domain/map/service/FindDetailClassByNameService.java
+++ src/main/java/com/pmn/gmt/domain/map/service/FindDetailClassByNameService.java
@@ -3,6 +3,8 @@ package com.pmn.gmt.domain.map.service;
 import com.pmn.gmt.domain.map.presentation.data.dto.DetailClassDto;
 import com.pmn.gmt.domain.map.presentation.data.dto.ClassNameDto;
 
+import java.util.List;
+
 public interface FindDetailClassByNameService {
-    DetailClassDto execute(ClassNameDto classNameDto);
+    List<DetailClassDto> execute(ClassNameDto classNameDto);
 }
